8f35db63489f43387fc5c89beaa1cccfbbb50847
Todd Stoneberg <todd.stoneberg@gmail.com>
2021-03-13 19:25:06 -0700

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+
+
+function writeCards(names) {
+    for (let i = 0; i < names.length; i++) {
+        names[i] = `Thank you, ${names[i]}, for the wonderful surprise gift!`
+    }
+    return names
+}
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n)
+        n--
+    }
+}
